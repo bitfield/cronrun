42bcecfc6e14c2520da3d17fba5efead7254ecff
John Arundel <john@bitfieldconsulting.com>
2018-05-29 15:58:41 +0100

    Improve usage message

--- cmd/cronrun/main.go
+++ cmd/cronrun/main.go
@@ -10,15 +10,22 @@ import (
 	"github.com/bitfield/cronrun"
 )
 
-const usage = `cronrun is a tool for running scheduled jobs specified by a single-line file in crontab format.
+const usage = `cronrun is a tool for running scheduled jobs specified by a file in crontab format.
 
 Usage: cronrun FILE
+
+cronrun is designed to be called from cron every minute. If you need to run cron jobs, but don't have access to the system crontabs, or you'd prefer to manage cron jobs in a file in a Git repo, for example, then cronrun is for you.
 
 Example file format:
 
 */5 * * * * /usr/local/bin/backup
+00 01 * * * /usr/bin/security_upgrades
 
-Running cronrun on the example file will run /usr/local/bin/backup if the current minute is divisible by 5.
+Running cronrun on the example file will run /usr/local/bin/backup if the current minute is divisible by 5, and will run /usr/bin/security_upgrades if the time is 01:00.
+
+Example cron job to trigger cronrun on a given file:
+
+* * * * * /usr/local/bin/cronrun /var/www/mysite/crontab
 `
 
 func main() {
